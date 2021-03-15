ccbe237ec253f0601878508d7d80ebf491a719d3
DaniaAbughoush <ms1dodo@gmail.com>
2021-03-15 15:33:47 +0200

    add else if

--- JS/main.js
+++ JS/main.js
@@ -9,17 +9,23 @@ console.log(drawing);
 if (drawing.toLowerCase()=='yes')
 {
   alert('yes'+' '+userName+' '+'you got it right');
-} else{
-    alert('sorry you got this one wrong i like drawing');
-};
+} else if(drawing.toLowerCase()=='no') {
+  alert('sorry you got this one wrong i like drawing');
 
+} else { alert('please answer yes or no ');
+  
+};
+    
 
 let mycolor=prompt('do think i like white')
 if(mycolor.toLowerCase()=='no')
 {
   alert('sorry to the white but its not my faviorit');
-} else{
-    alert('you look you dont like white like me');
+} else if(mycolor.toLowerCase()=='yes'){
+  alert('you got this wrong,i dont like white');
+}
+else{alert('please answer yes or no ');
+   
 
 };
 
@@ -29,8 +35,13 @@ if(travel.toLowerCase()=='yes')
 {
    alert('yes'+' '+userName+' '+'hope we can travel together')
 }
+else if(travel.toLowerCase()=='no')
+{
+  alert('no i like travelling'+' '+userName+' '+'hope we can travel together') 
+}
 else{
-    alert('no i like travelling'+' '+userName+' '+'hope we can travel together')
+  alert('please answer yes or no ');
+   
 };
 
 
@@ -39,8 +50,12 @@ else{
 let math=prompt('am I good at maths or not');
 if(math.toLowerCase()=='no')
 {
-  alert('you got it,oh i should improve my self in math');
-}else{ alert('no the answer is no,sorry');
+  alert('you got it,oh i should improve my self in math');}
+  else if(math.toLowerCase()=='yes')
+  {
+    alert('no the answer is no,sorry');
+  }
+else{ alert('please answer yes or no ');
 
 }
 
@@ -52,9 +67,14 @@ let cooking=prompt(userName+' '+'what about cooking .am i good or not');
 {
    alert('cooking is fun you got this one wright');
 
-}else{
+}else if(cooking.toLowerCase()=='no')
+{
     alert('you got this one wrong,i like to cook');
-};
+}else
+{
+  alert('please answer yes or no ');
+}
+;
 alert('thanks alot'+' '+userName+' '+'for talking your time to get to know me hope next time i have the chance to know you');
 
 
