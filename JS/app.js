a30604a85e7d3154c36de03083ac0aaba6de32eb
DaniaAbughoush <ms1dodo@gmail.com>
2021-03-18 18:03:12 +0200

    let

--- JS/app.js
+++ JS/app.js
@@ -1,136 +1,136 @@
-/* eslint-disable no-cond-assign */
-'use strict';
-let userName=prompt('welcome,please tell us your name');
-alert('welcom'+' '+userName);
+// /* eslint-disable no-cond-assign */
+// 'use strict';
+// let userName=prompt('welcome,please tell us your name');
+// alert('welcom'+' '+userName);
 
-alert('now lets get to know each other by knowing some information about me please answer with yes or no');
+// alert('now lets get to know each other by knowing some information about me please answer with yes or no');
 
-let drawing=prompt('do you think i like drawing');
+// let drawing=prompt('do you think i like drawing');
 
-if (drawing.toLowerCase()==='yes')
-{
-  alert('yes'+' '+userName+' '+'you got it right');
-  var a=1;
-} else if(drawing.toLowerCase()==='no') {
-  alert('sorry you got this one wrong i like drawing');
-  var a=0;
-} else { alert('please answer yes or no ');
-  var a=0;
+// if (drawing.toLowerCase()==='yes')
+// {
+//   alert('yes'+' '+userName+' '+'you got it right');
+//   var a=1;
+// } else if(drawing.toLowerCase()==='no') {
+//   alert('sorry you got this one wrong i like drawing');
+//   var a=0;
+// } else { alert('please answer yes or no ');
+//   var a=0;
 
-}
-
-
-let mycolor=prompt('do think i like white');
-if(mycolor.toLowerCase()==='no')
-{
-  alert('sorry to the white but its not my faviorit');
-  var b=1;
-} else if(mycolor.toLowerCase()==='yes'){
-  alert('you got this wrong,i dont like white');
-  var b=0;
-
-}
-else{alert('please answer yes or no ');
-
-  var b=0;
-
-}
+// }
 
 
-let travel=prompt(userName+' '+'do you think i like travelling');
-if(travel.toLowerCase()==='yes')
-{
-  alert('yes'+' '+userName+' '+'hope we can travel together');
-  var d=1;
-}
-else if(travel.toLowerCase()==='no')
-{
-  alert('no i like travelling'+' '+userName+' '+'hope we can travel together');
-  var d=0;
-}
-else{
-  alert('please answer yes or no ');
-  var d=0;
-}
+// let mycolor=prompt('do think i like white');
+// if(mycolor.toLowerCase()==='no')
+// {
+//   alert('sorry to the white but its not my faviorit');
+//   var b=1;
+// } else if(mycolor.toLowerCase()==='yes'){
+//   alert('you got this wrong,i dont like white');
+//   var b=0;
+
+// }
+// else{alert('please answer yes or no ');
+
+//   var b=0;
+
+// }
 
 
-
-
-let math=prompt('am I good at maths or not');
-if(math.toLowerCase()==='no')
-{
-  alert('you got it,oh i should improve my self in math');
-  var c=1;
-}
-else if(math.toLowerCase()==='yes')
-{
-  alert('no the answer is no,sorry');
-  var c=0;
-}
-else{ alert('please answer yes or no ');
-  var c =0;
-}
-
-
-
-let cooking=prompt(userName+' '+'what about cooking .am i good or not');
-
-if(cooking.toLowerCase()==='yes')
-{
-  alert('cooking is fun you got this one right');
-  var g=1;
-
-}else if(cooking.toLowerCase()==='no')
-{
-  alert('you got this one wrong,i like to cook');
-  var g =0;
-}else
-{
-  alert('please answer yes or no ');
-  var g=0;
-}
-
-alert('thanks alot'+' '+userName+' '+'for talking your time to get to know me hope next time i have the chance to know you');
+// let travel=prompt(userName+' '+'do you think i like travelling');
+// if(travel.toLowerCase()==='yes')
+// {
+//   alert('yes'+' '+userName+' '+'hope we can travel together');
+//   var d=1;
+// }
+// else if(travel.toLowerCase()==='no')
+// {
+//   alert('no i like travelling'+' '+userName+' '+'hope we can travel together');
+//   var d=0;
+// }
+// else{
+//   alert('please answer yes or no ');
+//   var d=0;
+// }
 
 
 
 
-
-for (let i = 0; i < 4; i++)
-{let guess=prompt('please guess anumber in my mind from 0 to 10'+' '+userName);
-
-
-  if (guess==='7') { alert('correct'+' '+'you got it'+' '+userName);
-    var e=1;
-    break;
-
-  } else if (guess<=7){ alert('too low try again'+' '+userName);
-    var e=0;
-
-  }else{alert('high number try '+' '+userName);
-    var e=0;
-
-  }}
-
-alert('correct answer is 7');
-let drinks = ['orange','water','tea','apple','coffe','mango','lemon'];
+// let math=prompt('am I good at maths or not');
+// if(math.toLowerCase()==='no')
+// {
+//   alert('you got it,oh i should improve my self in math');
+//   var c=1;
+// }
+// else if(math.toLowerCase()==='yes')
+// {
+//   alert('no the answer is no,sorry');
+//   var c=0;
+// }
+// else{ alert('please answer yes or no ');
+//   var c =0;
+// }
 
 
-for( let x=0 ; x<6 ;x++)
-{ let favDrink=prompt('Now guess my faviourit drink');
+
+// let cooking=prompt(userName+' '+'what about cooking .am i good or not');
+
+// if(cooking.toLowerCase()==='yes')
+// {
+//   alert('cooking is fun you got this one right');
+//   var g=1;
+
+// }else if(cooking.toLowerCase()==='no')
+// {
+//   alert('you got this one wrong,i like to cook');
+//   var g =0;
+// }else
+// {
+//   alert('please answer yes or no ');
+//   var g=0;
+// }
+
+// alert('thanks alot'+' '+userName+' '+'for talking your time to get to know me hope next time i have the chance to know you');
 
 
-  if (favDrink===drinks[1] || favDrink===drinks[2]|| favDrink===drinks[3] || favDrink===drinks[4] || favDrink===drinks[5] || favDrink===drinks[6] || favDrink===drinks[7] )
-  {alert(' you got this one right');
-    var f=1;
-    break;
 
-  } else {alert('nope!');
-    var f =0;
 
-  }}
 
-alert('the drinks that i like is'+' '+drinks);
+// for (let i = 0; i < 4; i++)
+// {let guess=prompt('please guess anumber in my mind from 0 to 10'+' '+userName);
 
-let y =a+b+d+c+e+f+g;
-confirm('your score is'+' '+y+' '+'out of 7');
+
+//   if (guess==='7') { alert('correct'+' '+'you got it'+' '+userName);
+//     var e=1;
+//     break;
+
+//   } else if (guess<=7){ alert('too low try again'+' '+userName);
+//     var e=0;
+
+//   }else{alert('high number try '+' '+userName);
+//     var e=0;
+
+//   }}
+
+// alert('correct answer is 7');
+// let drinks = ['orange','water','tea','apple','coffe','mango','lemon'];
+
+
+// for( let x=0 ; x<6 ;x++)
+// { let favDrink=prompt('Now guess my faviourit drink');
+
+
+//   if (favDrink===drinks[1] || favDrink===drinks[2]|| favDrink===drinks[3] || favDrink===drinks[4] || favDrink===drinks[5] || favDrink===drinks[6] || favDrink===drinks[7] )
+//   {alert(' you got this one right');
+//     var f=1;
+//     break;
+
+//   } else {alert('nope!');
+//     var f =0;
+
+//   }}
+
+// alert('the drinks that i like is'+' '+drinks);
+
+// let y =a+b+d+c+e+f+g;
+// confirm('your score is'+' '+y+' '+'out of 7');
